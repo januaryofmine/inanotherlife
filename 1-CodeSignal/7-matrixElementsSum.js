22c90a31430aee974f841ae2d1a1b93b12798ac0
KhanhTiet <khanh.tiet.6001@gmail.com>
2021-01-18 23:49:22 +0700

    longest string

--- 1-CodeSignal/7-matrixElementsSum.js
+++ 1-CodeSignal/7-matrixElementsSum.js
@@ -1,24 +1,34 @@
-function matrixElementsSum(matrix) {
-  let totalPoint = 0;
-  let isValidToAdd = true;
-  matrix = matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
-  matrix.forEach((row, rowIndex) => {
-    isValidToAdd = true;
-    row.forEach((el, i) => {
-      if (el == 0) {
-        isValidToAdd = false;
-      } else if (isValidToAdd) {
-        totalPoint += el;
-      }
-    });
-  });
-  return totalPoint;
-}
+// function matrixElementsSum(matrix) {
+//   let totalPoint = 0;
+//   let isValidToAdd = true;
+//   matrix = matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
+//   matrix.forEach((row, rowIndex) => {
+//     isValidToAdd = true;
+//     row.forEach((el, i) => {
+//       if (el == 0) {
+//         isValidToAdd = false;
+//       } else if (isValidToAdd) {
+//         totalPoint += el;
+//       }
+//     });
+//   });
+//   return totalPoint;
+// }
 
 const matrix = [
   [0, 1, 1, 2],
   [0, 5, 0, 0],
   [2, 0, 3, 3],
 ];
+
+// function matrixElementsSum(matrix) {
+//   for (var r = 0, j = 0; j < matrix[0].length; j++) {
+//     for (var i = 0; i < matrix.length; i++) {
+//       if (matrix[i][j] === 0) break;
+//       else r += matrix[i][j];
+//     }
+//   }
+//   return r;
+// }
 
 console.log(matrixElementsSum(matrix));
---
+++ 1-CodeSignal/8-allLongestStrings.js
@@ -0,0 +1,43 @@
+// function allLongestStrings(inputArray) {
+//   inputArray.reduce(
+//     (acc, value, i) => {
+//         console.log(i);
+//         console.log(acc);
+//         console.log(value);
+//         i == 0
+//         ? acc
+//         : value.length > acc[0].length
+//         ? [value]
+//         : value.length == acc[0].length
+//         ? [...acc, value]
+//         : acc;
+//         console.log(acc);
+//     },
+//     [inputArray[0]]
+//   );
+// }
+
+function allLongestStrings(inputArray) {
+  let rsArr = [];
+  inputArray.forEach((string, i) => {
+    if (i == 0) {
+      rsArr.push(string);
+    } else if (string.length > rsArr[0].length) {
+      rsArr = [];
+      rsArr.push(string);
+    } else if (string.length == rsArr[0].length) {
+      rsArr.push(string);
+    }
+  });
+  return rsArr;
+}
+
+// function allLongestStrings(inputArray) {
+//   "use strict";
+//   let maxSize = Math.max(...inputArray.map((x) => x.length));
+//   return inputArray.filter((x) => x.length === maxSize);
+// }
+
+const inputArray = ["aba", "aa", "ad", "vcd", "aba"];
+
+console.log(allLongestStrings(inputArray));
